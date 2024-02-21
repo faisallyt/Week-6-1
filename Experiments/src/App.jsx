d92dc3d5b60fd114bd3fcdf7087daeab3bd2d2cb
faisallyt <96649676+faisallyt@users.noreply.github.com>
2024-02-22 03:44:09 +0530

    CardWrapper

--- Experiments/src/App.jsx
+++ Experiments/src/App.jsx
@@ -1,58 +1,13 @@
-import { useState } from "react";
-import "./App.css";
-
-let counter = 4;
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "go to gym",
-      description: "go to gym today",
-    },
-    {
-      id: 2,
-      title: "ead food",
-      description: "eat your  food",
-    },
-    {
-      id: 1,
-      title: "go to class",
-      description: "go to class  ",
-    },
-  ]);
-  function addTodo() {
-    setTodos([
-      ...todos,
-      {
-        id: counter++,
-        title: Math.random(),
-        description: Math.random(),
-      },
-    ]);
-  }
   return (
     <div>
-      <input type="text" placeholder="title" />
-      <input type="text" placeholder="description" />
-      <button onClick={addTodo}>Add a todo Here</button>
-
-      {todos.map((todo) => (
-        <Todo
-          title={todo.title}
-          description={todo.description}
-          key={todo.id}></Todo>
-      ))}
+      <CardWrapper>Hii there</CardWrapper>
     </div>
   );
 }
 
-function Todo({ title, description }) {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
-    </div>
-  );
+function CardWrapper({ children }) {
+  return <div style={{ border: "2px solid black" }}>{children}</div>;
 }
 
 export default App;
