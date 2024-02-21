09d3958d1fdc8ecf0e6299dab7a8cdc97fcfcbd6
faisallyt <96649676+faisallyt@users.noreply.github.com>
2024-02-22 04:08:08 +0530

    Todo

--- Experiments/src/App.jsx
+++ Experiments/src/App.jsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [todo, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://sum-server.100xdevs.com/todos").then(async (res) => {
+      const data = await res.json();
+      console.log(data);
+      setTodos(data.todos);
+    });
+  }, []);
   return (
     <div>
-      <CardWrapper>Hii there</CardWrapper>
+      {todo.map((todo) => (
+        <div key={todo.id}>
+          <h1>{todo.title}</h1>
+          <h2>{todo.description}</h2>
+        </div>
+      ))}
     </div>
   );
-}
-
-function CardWrapper({ children }) {
-  return <div style={{ border: "2px solid black" }}>{children}</div>;
 }
 
 export default App;
